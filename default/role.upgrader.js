052145b627250658d71e7dfa24d7f6a543ee7513
G <pguth66@gmail.com>
2017-08-10 23:49:50 -0700

    fix for logic

--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -22,9 +22,9 @@ var roleUpgrader = {
 			var source;
 			var sources = creep.room.find(FIND_STRUCTURES, {
                  filter: (structure) => {
-						return ((structure.structureType == STRUCTURE_CONTAINER ||
+						return (structure.structureType == STRUCTURE_CONTAINER ||
 								structure.structureType == STRUCTURE_STORAGE) 
-								&& structure.store[RESOURCE_ENERGY] > 0);
+								&& (structure.store[RESOURCE_ENERGY] > 0);
                     }
 				});
 			if(sources.length == 0) {
