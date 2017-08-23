9a1171640972e199ee658ccac7b6684dd38c5d77
G <pguth66@gmail.com>
2017-08-22 22:55:57 -0700

    fill spawns first updates to pick up non-energy resources

--- default/role.hauler.js
+++ default/role.hauler.js
@@ -12,11 +12,11 @@ module.exports = {
     run: function(creep) {
         // find containers with energy, bring them to fill spawns and extensions
         
-        if((creep.memory.hauling) && creep.carry.energy == 0) {
+        if((creep.memory.hauling) && _.sum(creep.carry) == 0) {
             creep.memory.hauling = false;
             creep.say("Harvest");
         }
-        if (!creep.memory.hauling && creep.carry.energy == creep.carryCapacity) {
+        if (!creep.memory.hauling && _.sum(creep.carry) == creep.carryCapacity) {
             creep.memory.hauling = true;
             creep.say("Haul");
         }
@@ -29,46 +29,68 @@ module.exports = {
                                 structure.energy < structure.energyCapacity;
                     }
             });
-            if(targets.length > 0) {
-                const target = creep.pos.findClosestByPath(targets);
-                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+            var target ;
+            var targetSpawns = _.remove(targets, function(o) { return o.structureType == STRUCTURE_SPAWN; });
+            
+            if(targetSpawns.length > 0) {
+                target = creep.pos.findClosestByPath(targetSpawns);
+            }
+            else {
+                if(targets.length > 0) {
+                    target = creep.pos.findClosestByPath(targets);
                 }
             }
+            if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            
             else {
                 var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => { 
                         return  (structure.structureType == STRUCTURE_STORAGE ||
                                 structure.structureType == STRUCTURE_CONTAINER) &&
-                                (structure.store[RESOURCE_ENERGY] < (structure.storeCapacity /4)) ; }
+                                (structure.store[RESOURCE_ENERGY] < (structure.storeCapacity / 3)) ; }
                 });
                 if (targets.length > 0) {
                     const target = creep.pos.findClosestByPath(targets);
                     if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(target, {visualizePathStyle: {}});
                     }
+                   else {
+                    for(const resourceType in creep.carry) {
+                        if (creep.carry.resourceType > 0) {
+                            creep.transfer(target, resourceType);
+                        }
+                    }   
+                }
+
                 }
                 else {
-                    creep.moveTo(25,12);
+                    creep.moveTo(30,23);
                 }
             }
         }
 
         if(!creep.memory.hauling) {
- //           var sources = creep.room.find(FIND_DROPPED_RESOURCES);
- //           if(sources.length == 0) {
+            var sources = creep.room.find(FIND_DROPPED_RESOURCES);
+            if(sources.length == 0) {
                 sources = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (((structure.structureType == STRUCTURE_CONTAINER) || 
                             (structure.structureType == STRUCTURE_STORAGE)) 
-                            && (structure.store[RESOURCE_ENERGY] > (structure.storeCapacity / 3)));
+                            && (structure.store[RESOURCE_ENERGY] > (structure.storeCapacity / 2)));
  //                           && (structure.store[RESOURCE_ENERGY] > 1800));
                      }
                 });
-  //          }
+           }
                 const source=creep.pos.findClosestByPath(sources);
                 if(creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                    creep.moveTo(source, {visualizePathStyle: {}});
+                }
+                else {
+                    if(creep.pickup(source) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source, {visualizePathStyle: {}});
+                    }
                 }
             }
     
