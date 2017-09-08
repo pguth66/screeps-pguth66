cd1d7fc7cf40f6bb04fb951e6eb7d956e78b5853
G <pguth66@gmail.com>
2017-09-08 00:24:04 -0700

    added attacking hostile controllers

--- default/role.claimer.js
+++ default/role.claimer.js
@@ -18,17 +18,42 @@ var roleClaimer = {
             creep.moveTo(exit, {visualizePathStyle: {}});
         }
         else {
-            switch(creep.claimController(creep.room.controller)) {
-                case ERR_NOT_IN_RANGE:
-                    creep.moveTo(creep.room.controller, {visualizePathStyle: {}});
-                    break;
-                case OK:
-                    break;
-                default:
-                   if(creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                      creep.moveTo(creep.room.controller, {visualizePathStyle: {}});
-                    }   
-                    break;
+            if(creep.room.controller.owner != null) {
+                switch(creep.attackController(creep.room.controller)) {
+                    case ERR_NOT_IN_RANGE:
+                        creep.moveTo(creep.room.controller, {visualizePathStyle: {}});
+                        break;
+                    case ERR_INVALID_TARGET:
+                        creep.say('invtarget');
+                        break;
+                    case ERR_NOT_OWNER:
+                        console.log('not owner of controller');
+                        break;
+                    case ERR_NO_BODYPART:
+                        console.log('no bodypart');
+                        break;
+                    case OK:
+                        break;
+                    default:
+                        creep.say('Bazinga');
+                        break;
+                }
+            }
+            else {
+                switch(creep.claimController(creep.room.controller)) {
+                    case ERR_NOT_IN_RANGE:
+                        creep.moveTo(creep.room.controller, {visualizePathStyle: {}});
+                        break;
+                    case OK:
+                        break;
+                    case ERR_GCL_NOT_ENOUGH:
+                    if(creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.controller, {visualizePathStyle: {}});
+                        }   
+                        break;
+                    default:
+                        creep.say('claimErr');
+                }
             }
         }
 	}
