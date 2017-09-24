af9e0f5088d2d1dd89364cfafc1203b027de91af
G <pguth66@gmail.com>
2017-09-23 22:54:51 -0700

    don't attack ramparts or controllers prioritize towers added memory flag for room to attack

--- default/role.warrior.js
+++ default/role.warrior.js
@@ -19,12 +19,25 @@ var roleWarrior = {
 
         const hostileCreeps = creep.room.find(FIND_HOSTILE_CREEPS);
         const hostileStructures = creep.room.find(FIND_HOSTILE_STRUCTURES);
+        
 
         hostiles = hostileCreeps.concat(hostileStructures);
+        hostileTowers = _.remove(hostiles, {structureType: STRUCTURE_TOWER});
+        _.remove(hostiles, {structureType: STRUCTURE_RAMPART}); 
+        _.remove(hostiles, {structureType: STRUCTURE_CONTROLLER}); 
+        
+
+        // need to prioritize towers here, and attack/heal creeps
 
         // console.log(hostiles.length);
 
-        var closestHostile = creep.pos.findClosestByRange(hostiles);
+        if(hostileTowers.length > 0) {
+            var closestHostile = creep.pos.findClosestByPath(hostileTowers);
+        }
+        else {
+            var closestHostile = creep.pos.findClosestByPath(hostiles);
+        }
+    
         if(closestHostile) {
             switch(creep.attack(closestHostile)) {
                 case ERR_NOT_IN_RANGE: 
@@ -41,8 +54,16 @@ var roleWarrior = {
             }
         }   
         else {
-            //creep.moveTo(24,24, {visualizePathStyle: {}});
-            creep.memory.role='recycle';
+            if(Memory.roomToAttack) {
+                creep.say("ToROom");
+                const exitDir=creep.room.findExitTo(Memory.roomToAttack);
+                const exit=creep.pos.findClosestByRange(exitDir);
+                creep.moveTo(exit);
+            }
+            else {
+                //creep.moveTo(24,24, {visualizePathStyle: {}});
+                creep.memory.role='recycle';
+            }
         }
     }
 };
