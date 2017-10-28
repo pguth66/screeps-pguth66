ab11aa48906f12fe6ab3f54e6f53033612143dd2
G <pguth66@gmail.com>
2017-10-28 11:26:17 -0700

    Added creep.moveToTarget moved creep loop into separate module

--- default/main.js
+++ default/main.js
@@ -1,22 +1,5 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleHealer = require('role.healer');
-var roleHauler = require('role.hauler');
-var roleClaimer = require('role.claimer');
-var roleWarrior = require('role.warrior');
-var roleMiner = require('role.miner');
-var roleRecycle = require('role.recycle');
-var roleInterHauler = require('role.interhauler');
-var roleMinHauler = require('role.minhauler');
-var roleRemoteworker = require('role.remoteworker');
-var roleCalTrans = require('role.caltrans');
-var roleVanguard = require('role.vanguard');
-var roleScavenger = require('role.scavenger');
-var rolePatrol = require('role.patrol');
-var harvestRole = require('func.harvestRoles');
-var roleDismantle = require('role.dismantle');
-var roleContractHauler = require('role.contracthauler');
+
+var creepHandler = require('creepHandler');
 
 module.exports.loop = function () {
 
@@ -134,6 +117,7 @@ module.exports.loop = function () {
         var body = [];
         var newCreepMemory = { role: this.memory.role, respawn: true } ;
 
+        try {
         switch (this.memory.role) {
             case 'dismantle':
                 body = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
@@ -167,6 +151,10 @@ module.exports.loop = function () {
         } else {
             return false;
         }
+    }
+    catch (err) {
+        this.creepLog(err);
+    }
     }
     Creep.prototype.findAnyDepositTarget = function () {
         var roomMap = Memory.roomMaps[this.room.name];
@@ -434,60 +422,11 @@ module.exports.loop = function () {
         // grab an arry of all creeps in this room
         const roomCreeps = _.filter(Game.creeps, function (creep) { return creep.room.name == room.name });
 
-        const creepRoles = [{ role: 'harvester', run: roleHarvester.run },
-        { role: 'upgrader', run: roleUpgrader.run },
-        { role: 'healer', run: roleHealer.run },
-        { role: 'hauler', run: roleHauler.run },
-        { role: 'claimer', run: roleClaimer.run },
-        { role: 'warrior', run: roleWarrior.run },
-        { role: 'builder', run: roleBuilder.run },
-        { role: 'recycle', run: roleRecycle.run },
-        { role: 'miner', run: roleMiner.run },
-        { role: 'interhauler', run: roleInterHauler.run },
-        { role: 'minhauler', run: roleMinHauler.run },
-        { role: 'caltrans', run: roleCalTrans.run },
-        { role: 'vanguard', run: roleVanguard.run },
-        { role: 'scavenger', run: roleScavenger.run },
-        { role: 'patrol', run: rolePatrol.run },
-        { role: 'dismantle', run:roleDismantle.run},
-        { role: 'contracthauler', run:roleContractHauler.run},
-        { role: 'remoteworker', run: roleRemoteworker.run }
-        ];
-        //    console.log('role: ' + creepMap[0].role + " function: " + creepMap[0].run);
-
-
-        // loop over creeps in room, have them run the right role based on creepRoles
+        // loop over creeps in room, send them through the creepHandler
         for (var name in roomCreeps) {
-            var creep = roomCreeps[name];
-            if (!creep.spawning) {
-                if (creep.hasOnlyMoveParts()) {
-                    creep.creepLog('has only move parts, recycling')
-                    creep.memory.role = 'recycle';
-                }
-                if (creep.memory.targetRoom && (creep.memory.targetRoom != creep.room.name)) {
-                    creep.moveToRoom(creep.memory.targetRoom);
-                }
-                else {
-                    for (i = 0; i < creepRoles.length; i++) {
-                        if (creep.memory.role == creepRoles[i].role) {
-                            creepRoles[i].run(creep);
-                        }
-                    }
-                    // renew
-                    if (creep.memory.respawn && (creep.ticksToLive < 100)) {
-                        creep.say("Respawning");
-                        if (creep.respawn()) {
-                            creep.memory.respawn=false;
-                        }
-                    }
-                }
-            }
-            else { // initialization stuff
-                if (!creep.memory.spawnRoom) {
-                    creep.memory.spawnRoom = room.name;
-                }
-            }
-        } // end creepRuns
+            //var creep = roomCreeps[name];
+            creepHandler.handleCreep(roomCreeps[name]);
+        }
 
         if (roomOwner == 'MixtySix') {
             // start stage defaults
