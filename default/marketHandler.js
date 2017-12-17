f1fd73689c4c91678b18329ceef9323ca426bbf2
G <pguth66@gmail.com>
2017-12-17 08:50:16 -0800

    added creating orders pick lowest price if you have too much in terminal

--- default/marketHandler.js
+++ default/marketHandler.js
@@ -10,6 +10,7 @@
  // TODOs
  // better average calculation to avoid gaming with amount = 1 transactions
  // handle compounds elements (not just X, U, etc)
+ // if terminal is full, set price at lowest possible
 
  module.exports = {
 
@@ -18,11 +19,17 @@
         function getAveragePrice(mineralType) {
             //console.log('computing average price of ' + mineralType);
             const orders = Game.market.getAllOrders({resourceType: mineralType});
+            const amountinTerminal = room.terminal.store[mineralType];            
             // pull out just the price
             const prices = orders.map(function (order) { return order.price}).sort();
             //console.log(JSON.stringify(prices,null,4));
             // for now just pick the median, need to make this more sophisticated
-            return prices[Math.round(prices.length / 3)];
+            if (amountinTerminal < 190000) {
+                return prices[Math.round(prices.length / 3)];
+            }
+            else {
+                return prices[0];
+            }
         }
 
         function processOrder (order, room) {
@@ -32,6 +39,8 @@
             const floor = 0.15 ;
 
             const averagePrice = getAveragePrice(mineralType);
+            const amountinTerminal = room.terminal.store[mineralType];
+            
             //console.log('average price of ' + mineralType + ' is ' + averagePrice )
 
             var price = averagePrice < floor ? floor : averagePrice - 0.005 ;
@@ -43,10 +52,11 @@
             }
 
             if (order.remainingAmount < 10000) {
-                const amountinTerminal = room.terminal.store[mineralType];
                 var amount = amountinTerminal < 60000 ? amountinTerminal - 10000 : 50000 ;
-                console.log(room.name + " wants to extend order by " + amount );
-                Game.market.extendOrder(order.id, amount);
+                if (amount > 0) {
+                    console.log(room.name + " wants to extend order by " + amount );
+                    Game.market.extendOrder(order.id, amount);
+                }
             }
         }
 
@@ -73,7 +83,7 @@
         var orders = _.filter(Game.market.orders, {roomName: room.name, type: ORDER_SELL});
         switch (orders.length) {
             case 0:
-                return;
+                Game.market.createOrder(ORDER_SELL, mineralType, getAveragePrice(mineralType),1,room.name);
                 break;
             case 1:
                 //console.log('doing orders in ' + room.name);
