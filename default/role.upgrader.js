e692df74e36c8b746132440c339c4fbad5aa7608
G <pguth66@gmail.com>
2018-01-02 01:27:21 -0800

    don't try to upgrade when it's not my room

--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -16,7 +16,7 @@ var roleUpgrader = {
 			}
 	    }
 
-	    if(creep.memory.upgrading) {
+	    if(creep.room.controller.my && creep.memory.upgrading) {
 			switch(creep.upgradeController(creep.room.controller)) {
 				case ERR_NOT_IN_RANGE: 
 					creep.moveTo(creep.room.controller, {visualizePathStyle: {}});
