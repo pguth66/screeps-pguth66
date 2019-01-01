7c1265522384b7d936be86aba655d0a8a8eafb67
G <pguth66@gmail.com>
2018-12-31 19:39:42 -0800

    added junkyard hauling to contracthaulers (only drop)

--- default/role.contracthauler.js
+++ default/role.contracthauler.js
@@ -20,7 +20,17 @@ module.exports = {
         // call it stopAfter - when you deposit, increment counter by your carry
 
         const pt = Game.getObjectById(creep.memory.pullTarget);
-        const dt = Game.getObjectById(creep.memory.dropTarget);
+        var dt = null;
+        // need this special case to handle junkyards, which don't have ids to store in memory
+        // because they are just created with "new RoomObject"
+        switch (creep.memory.dropTarget) {
+            case 'junkyard': 
+                dt = creep.room.junkyard;
+                break;
+            default:
+                dt = Game.getObjectById(creep.memory.dropTarget);
+        }
+
         const resourceType = creep.memory.resource;
 
         try{
@@ -48,50 +58,68 @@ module.exports = {
                 else {
                     creep.moveTo(pt);
                 }
-                switch (dt.structureType) {
-                    case STRUCTURE_NUKER:
-                        if (resourceType == RESOURCE_ENERGY && dt.energy >= creep.memory.upTo) {
-                            creep.memory.role='pause';
-                        }
-                        else {
-                            if (resourceType == RESOURCE_GHODIUM && dt.ghodium >= creep.memory.upTo) {
-                                creep.memory.role='pause'
-                            }
-                        }
-                        break;
-                    case STRUCTURE_LAB:
-                        if (resourceType == RESOURCE_ENERGY) {
-                            if (dt.energy >= creep.memory.upTo) {
-                                creep.memory.role='pause';
-                            }
-                        }
-                        else {
-                            if (dt.mineralAmount >= creep.memory.upTo) {
-                                creep.memory.role='pause;'
-                            }
-                        }
-                        break;
-                    case STRUCTURE_POWER_SPAWN:
-                        if (resourceType == RESOURCE_POWER && dt.power >= creep.memory.upTo) {
-                            creep.memory.role='pause';
-                        }
-                        else {
+                if (creep.memory.dropTarget == 'junkyard') {
+                    if (creep.memory.processed >= creep.memory.total) {
+                        creep.memory.role='pause';
+                    }
+                }
+                else {
+                    switch (dt.structureType) {
+                        case STRUCTURE_NUKER:
                             if (resourceType == RESOURCE_ENERGY && dt.energy >= creep.memory.upTo) {
                                 creep.memory.role='pause';
                             }
-                        }
-                        break;
-                    default:
-                        if ((dt.store[resourceType] >= creep.memory.upTo) || 
-                            (creep.memory.processed >= creep.memory.total)) {
-                            creep.memory.role='pause';
-                            creep.memory.respawn=false;
-                        }
-                        break;
+                            else {
+                                if (resourceType == RESOURCE_GHODIUM && dt.ghodium >= creep.memory.upTo) {
+                                    creep.memory.role='pause'
+                                }
+                            }
+                            break;
+                        case STRUCTURE_LAB:
+                            if (resourceType == RESOURCE_ENERGY) {
+                                if (dt.energy >= creep.memory.upTo) {
+                                    creep.memory.role='pause';
+                                }
+                            }
+                            else {
+                                if (dt.mineralAmount >= creep.memory.upTo) {
+                                    creep.memory.role='pause;'
+                                }
+                            }
+                            break;
+                        case STRUCTURE_POWER_SPAWN:
+                            if (resourceType == RESOURCE_POWER && dt.power >= creep.memory.upTo) {
+                                creep.memory.role='pause';
+                            }
+                            else {
+                                if (resourceType == RESOURCE_ENERGY && dt.energy >= creep.memory.upTo) {
+                                    creep.memory.role='pause';
+                                }
+                            }
+                            break;
+                        default:
+                            if ((dt.store[resourceType] >= creep.memory.upTo) || 
+                                (creep.memory.processed >= creep.memory.total)) {
+                                creep.memory.role='pause';
+                                creep.memory.respawn=false;
+                            }
+                            break;
+                    }
                 }
             } else {
-//                if (creep.pos.inRangeTo(dt,1)) {
-                    const r = creep.carry[resourceType];
+                // not hauling
+                const r = creep.carry[resourceType];
+                if (creep.memory.dropTarget == 'junkyard' && creep.room.junkyard) {
+                    if (creep.pos.isEqualTo(dt.pos)) {
+                        creep.say("DROP");
+                        creep.drop(resourceType);
+                        creep.memory.processed += r;
+                        creep.memory.hauling = false;
+                    } else {
+                        creep.moveTo(dt);
+                    }
+                }
+                else {
                     switch (creep.transfer(dt, resourceType)) {
                         case OK:
                             creep.say('Deposit');   
@@ -108,11 +136,7 @@ module.exports = {
                         default:
                             creep.creepLog('error transferring ' + resourceType + ' to dropTarget ' + dt.id);
                     }
-//                }
- //               else {
- //                   //creep.say('Moving');
-//                    creep.moveTo(dt);
-//                }
+                }
             }
         }
         catch(err) {
