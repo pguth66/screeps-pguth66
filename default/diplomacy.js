e11fbc3cea4dede23e5f828cc60ac548f00de3ea
G <pguth66@gmail.com>
2019-11-26 19:46:04 -0800

    working squads through init state

--- default/diplomacy.js
+++ default/diplomacy.js
@@ -18,24 +18,60 @@ Room.prototype.hasFullNuker = function () {
     }
 }
 
+/**
+ * Class to represent squads
+ * @constructor
+ * @param {string} spawnRoom - name of the room where squad soldiers should be spawned
+ * @param {string[]} members - array of soldier types to make up the squad
+ * @param {string} mission - type of mission
+ * @param {string} target - describes the target of the mission
+ * @param {string} targetRoom - name of the room where the mission is
+ * @param {number} id - essentially the primary key for the squads table in memory
+ * @param {string} state - FSM state of the squad (comes from memory if not init)
+ * 
+ */
 class Squadron {
-    constructor (spawnRoom,members,mission,target,targetRoom,id) {
+    constructor (spawnRoom,members,mission,target,targetRoom,id,state) {
         this.spawnRoom = spawnRoom; // room name, not room object
-        this.members = members;
+        this.members = members; // array of roles 
+        // memberState = [ init, queued, spawned ]
         this.mission = mission;
         this.target = target;
         this.targetRoom = targetRoom; // note you need to have the Room object (via observer) or this gets weird
         this.id = id;
-        this.state = 'init';
+        this.state = state;
+
+        this.serialNumberPattern = 'squad-' + id + '-'; // used to construct member IDs
+
+        this.members.forEach(function (member, i) {
+            //console.log (JSON.stringify(member) + ' ' + i)
+            return this.members[i] = {role:member,id:this.serialNumberPattern  + member + '-' + i, memberState: 'init'};
+        },this)
+
+        //console.log('squad ' + this.id + ' has member length ' + members.length);
 
         // this Memory object is used to persist the squad across ticks
-        Memory.squads[this.id] = {};
-        const properties = [ 'id', 'spawnRoom', 'members', 'mission', 'target', 'targetRoom', 'state'];
+        // if it doesn't exist, create it and store stuff
+        if (!Memory.squads[this.id]) {
+            console.log('init memory for ' + this.id);
+            Memory.squads[this.id] = {};
+            this.state = 'init';
+            this.saveState();
+            console.log(spawnRoom + ' creating squadron of ' + members.length);
+        }
+    } // end constructor
+
+    saveState() {
+        const properties = [ 'id', 'spawnRoom', 'mission', 'target', 'targetRoom', 'state'];
         properties.forEach(function(p) {
             //console.log(p);
             Memory.squads[this.id][p] = this[p];
         },this);
-        console.log(spawnRoom + ' creating squadron of ' + members.length);
+        Memory.squads[this.id].members = [];
+        this.members.forEach(function (m,i) {
+            console.log('storing memory for ' + this.id + ' member ' + m.id)
+            Memory.squads[this.id]['members'][i] = m;
+        },this);
     }
 }
 
@@ -50,41 +86,77 @@ module.exports = {
         }
         const currId = Memory.currentSquadNum;
 
-        if (!Memory.currentSquadNum == 1) {
-            testSquad = new Squadron('W29N29',['medic','warrior'],'attack','powerSpawn','W30N27',currId);
+        if (currId == 7) {
+            testSquad = new Squadron('W29N23',['testsquad','testsquad'],'attack','powerSpawn','W29N23',currId,'init');
             Memory.currentSquadNum += 1;
         }
+         if (currId == 5) {
+                testSquad2 = new Squadron('W29N23',['warrior','medic','warrior'],'attack','powerSpawn','W30N24',currId);
+                Memory.currentSquadNum += 1;
+        } 
 
 
         try {
         //process each squad
+        // rebuild the squad from Memory
+        // do its thing
+        // store the updated info back into Memory
         Object.keys(Memory.squads).forEach( function (key) {
-            const spawnRoom = Game.rooms[Memory.squads[key].spawnRoom]; // get the room object, for spawning
-            const targetRoom = Memory.squads[key].targetRoom; // room name, not object
+            // first rebuild the Squadron object from Memory
+            if (Memory.squads[key].state != 'inactive') {
+                //const properties = [ 'id', 'spawnRoom', 'members', 'mission', 'target', 'targetRoom', 'state'];
+                const id = key;
+                const spawnRoomName = Memory.squads[key].spawnRoom;
+                let members = Memory.squads[key].members;
+                //console.log(JSON.stringify(members[0]));
+/*                 members.forEach(function (member,i) {
+                    return members[i] = JSON.parse(member);
+                }); */
 
-            //console.log('squad ' + Memory.squads[key].id + ' has ' + Memory.squads[key].members.length + ' members')
-            switch (Memory.squads[key].state) {
-                case 'init':
-                    //console.log('init state');
-                    const spawnNamePattern = 'squad-' + Memory.squads[key].id + '-';
-                    //console.log(spawnNamePattern)
-                    Memory.squads[key].members.forEach(function (member, i) {
-                        const memberID = spawnNamePattern + member + '-' + i;
-                        if (spawnRoom.getTotalCreeps(member) == 0) {
-                            console.log('would spawn ' + memberID + ' with target ' + targetRoom);
-                            if (!spawnRoom.addToCreepBuildQueue(member,{targetRoom:targetRoom})) {
-                                console.log('spawning error');
-                                return;
+                let memberRoles = [];
+                members.forEach(function (m) {
+                    //console.log('role ' + m.role)
+                    memberRoles.push(m.role);
+                })
+                const mission = Memory.squads[key].mission;
+                const target = Memory.squads[key].target;
+                const targetRoom = Memory.squads[key].targetRoom;
+                const state = Memory.squads[key].state;
+
+                squad = new Squadron(spawnRoomName,memberRoles,mission,target,targetRoom,id,state)
+            
+                const spawnRoom = Game.rooms[Memory.squads[key].spawnRoom]; // get the room object, for spawning
+
+                //console.log('squad ' + Memory.squads[key].id + ' has ' + Memory.squads[key].members.length + ' members')
+                switch (squad.state) {
+                    case 'init':
+                        //console.log('init state');
+                        const spawnNamePattern = 'squad-' + squad.id + '-';
+                        //const spawnNamePattern = 
+                        //console.log(spawnNamePattern)
+                        squad.members.forEach(function (member, i) {
+                            console.log("member " + i + ' has state ' + member.memberState);
+                            // because dry runs work one creep at a time, need to only add one creep per turn to build queue
+                            if (member.memberState == 'init' && ((Game.time % squad.members.length) == i)) {
+                                if (!spawnRoom.hasCreepWithJob(member.id)) {
+                                    console.log('would spawn ' + member.id + ' with target ' + targetRoom);
+                                    if (!spawnRoom.addToCreepBuildQueue(member.role,{targetRoom:targetRoom,respawn:true,job:member.id})) {
+                                        console.log('spawning error');
+                                        return;
+                                    }
+                                    return members[i].memberState = 'queued';
+                                }
                             }
-                           Memory.squads[key].state = 'waitForSpawn';
-                        }
-                        //console.log(memberID);
-                    });
-                    break;
-                case 'waitForSpawn':
-                    break;
-                default:
-                    console.log('error processing squad ' + Memory.squads[key].id);
+                            //console.log(memberID);
+                        });
+                        squad.saveState();
+                        //Memory.squads[key].state = 'waitForSpawn';
+                        break;
+                    case 'waitForSpawn':
+                        break;
+                    default:
+                        console.log('error processing squad ' + squad.id);
+                }
             }
         })
     } catch(err) {
