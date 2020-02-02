ba578c94cfd9876ef5a96d87de35aeaea380908a
G <pguth66@gmail.com>
2020-02-01 20:49:07 -0800

    first file to switch to typescript

--- default/role.dismantle.js
+++ default/role.dismantle.js
@@ -6,34 +6,28 @@
  * var mod = require('role.dismantle');
  * mod.thing == 'a thing'; // true
  */
-
 var roleDismantle = {
-
-    run: function(creep) {
-
+    run: function (creep) {
         if (creep.room.memory.foundHostiles) {
             creep.moveTo(creep.room.controller);
             return;
         }
-
-        const dismantleFlags = creep.room.find(FIND_FLAGS, { filter: { color: COLOR_RED } });
+        var dismantleFlags = creep.room.find(FIND_FLAGS, { filter: { color: COLOR_RED } });
         if (dismantleFlags.length > 0) {
-            const dismantleFlag = creep.pos.findClosestByPath(dismantleFlags);
+            var dismantleFlag = creep.pos.findClosestByPath(dismantleFlags);
             var dismantleTargets = dismantleFlag.pos.lookFor(LOOK_STRUCTURES);
             if (dismantleTargets.length == 0) {
                 dismantleFlag.remove();
             }
             //console.log(room.name + dismantleTargets[0]);
-
             // should make this into an array, then towers can iterate over it and if it's empty won't cause problems
             if (dismantleTargets.length > 0) {
-                const dismantleTarget = dismantleTargets[0];
-
+                var dismantleTarget = dismantleTargets[0];
                 if (dismantleTarget.notifyWhenAttacked) {
                     dismantleTarget.notifyWhenAttacked(false);
                 }
                 if (creep.carry[RESOURCE_ENERGY] == creep.carryCapacity) {
-                    const depositTarget = creep.findAnyDepositTarget();
+                    var depositTarget = creep.findAnyDepositTarget();
                     if (creep.transfer(depositTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(depositTarget);
                     }
@@ -42,11 +36,9 @@ var roleDismantle = {
                     if (creep.dismantle(dismantleTarget) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(dismantleTarget);
                     }
-
                 }
             }
         }
     }
 };
-
-module.exports = roleDismantle ;
+module.exports = roleDismantle;
